bc2c0aee0d16f201dbe192eb46a1ab83bbe8a906
william <goodwong@foxmail.com>
2017-05-11 08:46:08 +0000

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# Laravel 5 Wechat OAuth
+
+微信网页登录，并且将微信资料存储到数据库
+
+## OAuth 中间件
+有两种方式设置中间件：
+
+- 设置 middleware
+    在app/Http/Kernel.php里添加：
+    ```php
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+
+        // ...
+
+        'wechat_oauth' => \Goodwong\LaravelWechatOAuth\Middleware\OAuthAuthenticate::class,
+    ];
+    ```
+
+- 直接在web.php的路由规则里添加：
+    ```php
+    // user auth
+    Route::group([
+        'middleware' => [
+            \Goodwong\LaravelWechatOAuth\Middleware\OAuthAuthenticate::class,
+        ],
+    ], function () {
+        // ...
+    });
+
+    ```
+
+## 配置
+在.env文件中，配置以下信息：
+
+- 微信平台
+    ```ini
+    WECHAT_APPID=
+    WECHAT_SECRET=
+    ```
+    > 这些信息可以在开放平台里注册获取
+
+
+
+
---
+++ src/Middleware/OAuthAuthenticate.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Goodwong\LaravelWechatOAuth\Middleware;
+
+use Log;
+use Closure;
+use Illuminate\Http\Request;
+use Goodwong\LaravelWechat\Handlers\CreateWechatUserHandler;
+use Goodwong\LaravelWechat\Events\WechatUserAuthorized;
+use Goodwong\LaravelWechat\Repositories\WechatUserRepository;
+use EasyWeChat\Foundation\Application as EasyWechatApplication;
+
+class OAuthAuthenticate
+{
+    /**
+     * construct
+     * 
+     * @param  CreateWechatUserHandler  $createWechatUserHandler
+     * @param  WechatUserRepository  $wechatUserRepository
+     * @return void
+     */
+    public function __construct(CreateWechatUserHandler $createWechatUserHandler, WechatUserRepository $wechatUserRepository)
+    {
+        $this->createWechatUserHandler = $createWechatUserHandler;
+        $this->wechatUserRepository = $wechatUserRepository;
+
+        $config = [
+            'app_id' => config('wechat.app_id'),
+            'secret' => config('wechat.secret'),
+        ];
+        $this->wechat = new EasyWechatApplication($config);
+    }
+
+    /**
+     * Handle an incoming request.
+     * 
+     * @param Request  $request
+     * @param \Closure  $next
+     * @param string|null  $scopes
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $scopes = null)
+    {
+        // 已登录用户
+        if ($request->user()) {
+            return $next($request);
+        }
+
+        // 非微信，跳过
+        if (!$this->isWeChatBrowser($request)) {
+            return $next($request);
+        }
+
+        $scopes = $scopes ?: config('wechat.oauth.scopes', 'snsapi_userinfo');
+        if (is_string($scopes)) {
+            $scopes = array_map('trim', explode(',', $scopes));
+        }
+
+        // 有缓存
+        $exist = session('wechat.oauth_user');
+        if ($exist) {
+            $wechatUser = $this->wechatUserRepository->find($exist['id']);
+            event(new WechatUserAuthorized($wechatUser));
+            return $next($request);
+        }
+
+        // 转去授权
+        if (!$request->has('code')) {
+            return $this->wechat->oauth->scopes($scopes)->redirect($request->fullUrl());
+        }
+
+        // 解析 code
+        $info = $this->wechat->oauth->user()->getOriginal();
+        Log::info('[wechat_login] original info: ', (array)$info);
+        $wechatUser = $this->wechatUserRepository->firstOrCreate(array_only($info, 'openid'), $info);
+        event(new WechatUserAuthorized($wechatUser));
+        session(['wechat.oauth_user' => $wechatUser]);
+
+        return redirect()->to($this->getTargetUrl($request));
+    }
+
+    /**
+     * Build the target business url.
+     *
+     * @param Request $request
+     *
+     * @return string
+     */
+    protected function getTargetUrl($request)
+    {
+        $queries = array_except($request->query(), ['code', 'state']);
+        return $request->url().(empty($queries) ? '' : '?'.http_build_query($queries));
+    }
+
+    /**
+     * Detect current user agent type.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return bool
+     */
+    protected function isWeChatBrowser($request)
+    {
+        return strpos($request->header('user_agent'), 'MicroMessenger') !== false;
+    }
+}
